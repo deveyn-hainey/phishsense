024a35cdc9d12c0a12de4a0237ed3bef127e693d
deveyn-hainey <deveynnhainey@gmail.com>
2025-02-26 15:32:21 -0700

    adding new format updates to AI tab

--- buildAiExplanationCard.js
+++ buildAiExplanationCard.js
@@ -1,16 +1,24 @@
 function buildAiExplanationCard() {
   var header = CardService.newCardHeader()
     .setTitle("PhishSense")
-    .setImageUrl("https://drive.google.com/uc?export=view&id=12bNfhfTQ_pNQtVQkPrzSkltX4HGVBFe");
+    .setImageUrl("https://drive.google.com/uc?export=view&id=12bNfhfTQ_pNQtVQkPrzSkltX4HGVBFed");
 
   // Another tab bar
   var tabSection = buildTabBar("aiExplanation");
 
   // Main section: show the explanation
   var explainSection = CardService.newCardSection()
-    .setHeader("<b>AI Explanation</b>");
+    .setHeader("<b>AI Explanation Test</b>");
 
   var explanation = getAiAnalysisFromCache();
+  explanation = explanation.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>");
+
+  explanation = formatExplanationForBullets(explanation);
+  //explanation = fixExplanationFormat(explanation);
+
+  // Convert newlines (\n) to <br> so they appear as line breaks
+  //explanation = explanation.replace(/\n/g, "<br>");
+
   if (!explanation) {
     explainSection.addWidget(
       CardService.newTextParagraph()
